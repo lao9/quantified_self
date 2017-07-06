70eae3f107a81e118855d40e4b83cbaafe0742fd
jk1dd <portajonk@gmail.com>
2017-07-06 14:28:27 -0600

    add fuctionality for deleting a food_meal [Finishes #148101403]

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express()
 var Food = require('./lib/models/food')
 var pry = require('pryjs')
 var bodyParser = require('body-parser')
+var Meal = require('./lib/models/meal')
+var FoodMeal = require('./lib/models/foodMeal')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -62,6 +64,18 @@ app.put('/api/foods/:id', function(request, response) {
   Food.updateFood(id, name, calories).then(function() {
     Food.findAllFoods().then(function(data){
       response.json(data.rows)
+    })
+  })
+})
+
+app.delete('/api/food_meals/:id', function(request, response) {
+  var id = request.params.id
+  FoodMeal.findMeal(id).then(function(result){
+    var mealId = result.rows[0].meal_id
+    FoodMeal.deleteById(id).then(function(){
+      Meal.findAllFoods(mealId).then(function(data){
+        response.json(data.rows)
+      })
     })
   })
 })
