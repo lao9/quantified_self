f3d9ed7d462a988ff74c53ec06e1e2370b6d48ed
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-11 12:26:45 -0600

    remove unneccessary cors stuff

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ var FoodMealRouter = require('./lib/routers/foodMealRouter')
 const cors = require('cors');
 
 app.use(cors({origin: '*'}))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
